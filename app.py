a8566921e88e36e08cc1aa816e811a1d5c4f61f0
Dylan <wdylan907@gmail.com>
2021-07-20 21:42:13 -0400

    edit env vars

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 import requests
-from config import API_KEY
+import os
+#from config import OPENWEATHER_KEY, MAPBOX_KEY
+
+OPENWEATHER_KEY = os.environ.get('OPENWEATHER_KEY')
+MAPBOX_KEY = os.environ.get('MAPBOX_KEY')
 
 app = Flask(__name__)
 
@@ -12,7 +16,7 @@ def index():
 def weather_data(lat, lng):
     URL = ('https://api.openweathermap.org/data/2.5/onecall?'
            'units=imperial&exclude=minutely,hourly'
-           f'&lat={lat}&lon={lng}&appid={API_KEY}')
+           f'&lat={lat}&lon={lng}&appid={OPENWEATHER_KEY}')
     return requests.get(URL).json()
 
 if __name__ == "__main__":
